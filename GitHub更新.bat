b06f4bd3b24aa5cb4ef348fef0f59d3a9dcae90d
alea5555 <83798106+alea5555@users.noreply.github.com>
2026-01-07 09:51:33 +0800

    remove obsolete scripts

---
+++ GitHub更新.bat
@@ -0,0 +1,11 @@
+cd F:\F\AI\Git
+git status
+git add -A
+git commit -m "remove obsolete scripts"
+git push
+cd F:\F\AI\public
+git status
+git add -A
+git commit -m "remove obsolete scripts"
+git push
+git status
